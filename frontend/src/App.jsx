2d8803f7c49d2cc9fe8d930931c2633192d3f4cb
Pedro Paulo <pedro.ppsilva09@gmail.com>
2026-05-25 22:05:44 -0300

    feat: telas de servicos (lista, detalhe e formulario)
    - lista em tabela com navegacao para o detalhe
    - detalhe com acoes de editar e desativar
    - formulario de criacao/edicao; registra as rotas no App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import ServicosList from './pages/servicos/ServicosList'
+import ServicoForm from './pages/servicos/ServicoForm'
+import ServicoDetail from './pages/servicos/ServicoDetail'
 
 export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+
+        <Route path="/servicos" element={<ServicosList />} />
+        <Route path="/servicos/novo" element={<ServicoForm />} />
+        <Route path="/servicos/:id" element={<ServicoDetail />} />
+        <Route path="/servicos/:id/editar" element={<ServicoForm />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
